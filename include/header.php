7e9344f7e39e3cd9c5390362181b5cd5eecb3cef
Jay Prakash OSFIGO <jayprakashutw@outlook.com>
2025-06-16 13:15:50 +0530

    FooterHeader
    Header and Footer Completed
    Admin and Student Dashboard

---
+++ include/header.php
@@ -0,0 +1,187 @@
+    <nav class="navbar navbar-expand-lg">
+        <div class="container px-0">
+            <a class="navbar-brand" href="index-2.html"><img src="assets/images/brand/logo/logo.svg" alt="Geeks" /></a>
+            <!-- Mobile view nav wrap -->
+            <div class="ms-auto d-flex align-items-center order-lg-3">
+                <div class="d-flex gap-2 align-items-center">
+
+                    <a href="pages/sign-in.html" class="btn btn-outline-dark">Login</a>
+                    <a href="pages/sign-up.html" class="btn btn-dark d-none d-md-block">Join Now</a>
+                </div>
+            </div>
+            <div>
+                <!-- Button -->
+                <button class="navbar-toggler collapsed ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-default" aria-controls="navbar-default" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="icon-bar top-bar mt-0"></span>
+        <span class="icon-bar middle-bar"></span>
+        <span class="icon-bar bottom-bar"></span>
+      </button>
+            </div>
+            <!-- Collapse -->
+            <div class="collapse navbar-collapse" id="navbar-default">
+                <ul class="navbar-nav mt-3 mt-lg-0 mx-xxl-auto">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarBrowse" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-bs-display="static">Courses</a>
+                        <ul class="dropdown-menu dropdown-menu-arrow" aria-labelledby="navbarBrowse">
+                            <li class="dropdown-submenu dropend">
+                                <a class="dropdown-item dropdown-list-group-item dropdown-toggle" href="#">Web Development</a>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a class="dropdown-item" href="pages/course-category.html">Bootstrap</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="pages/course-category.html">React</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="pages/course-category.html">GraphQl</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="pages/course-category.html">Gatsby</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="pages/course-category.html">Grunt</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="pages/course-category.html">Svelte</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="pages/course-category.html">Meteor</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="pages/course-category.html">HTML5</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="pages/course-category.html">Angular</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="dropdown-submenu dropend">
+                                <a class="dropdown-item dropdown-list-group-item dropdown-toggle" href="#">Design</a>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a class="dropdown-item" href="pages/course-category.html">Graphic Design</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="pages/course-category.html">Illustrator</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="pages/course-category.html">UX / UI Design</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="pages/course-category.html">Figma Design</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="pages/course-category.html">Adobe XD</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="pages/course-category.html" class="dropdown-item">Mobile App</a>
+                            </li>
+                            <li>
+                                <a href="pages/course-category.html" class="dropdown-item">IT Software</a>
+                            </li>
+                            <li>
+                                <a href="pages/course-category.html" class="dropdown-item">Digital Marketing</a>
+                            </li>
+                            <li>
+                                <a href="pages/course-category.html" class="dropdown-item">Database</a>
+                            </li>
+                            <li>
+                                <a href="pages/course-category.html" class="dropdown-item">Programming Language</a>
+                            </li>
+                            <li>
+                                <a href="pages/course-category.html" class="dropdown-item">Business</a>
+                            </li>
+                            <li>
+                                <a href="pages/course-category.html" class="dropdown-item">Office Utility</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="dropdown-item" href="pages/about.html">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="dropdown-item" href="pages/contact.html">Contact</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarPages" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pages</a>
+                        <ul class="dropdown-menu dropdown-menu-arrow" aria-labelledby="navbarPages">
+
+                            <li>
+                                <a class="dropdown-item" href="pages/dashboard-project.html">Projects
+              </a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="pages/dashboard-quiz.html">Quizzes 
+              </a>
+                            </li>
+                            <li class="dropdown-submenu dropend">
+                                <a class="dropdown-item dropdown-list-group-item dropdown-toggle" href="#">Paths</a>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a href="pages/course-path.html" class="dropdown-item">Browse Path</a>
+                                    </li>
+                                    <li>
+                                        <a href="pages/course-path-single.html" class="dropdown-item">Path Single</a>
+                                    </li>
+                                </ul>
+                            </li>
+
+
+                            <li class="dropdown-submenu dropend">
+                                <a class="dropdown-item dropdown-list-group-item dropdown-toggle" href="#">Career</a>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a class="dropdown-item" href="pages/career.html">Overview</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="pages/career-list.html">Listing</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="pages/career-single.html">Opening</a>
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li class="dropdown-submenu dropend">
+                                <a class="dropdown-item " href="pages/jobs/job-listing.html">Jobs</a>
+
+                            </li>
+
+
+
+
+                            <li class="dropdown-submenu dropend">
+                                <a class="dropdown-item dropdown-list-group-item dropdown-toggle" href="#">Help Center</a>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a class="dropdown-item" href="pages/help-center.html">Help Center</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="pages/help-center-faq.html">FAQ's</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="pages/help-center-guide.html">Guide</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="pages/help-center-guide-single.html">Guide Single</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="pages/help-center-support.html">Support</a>
+                                    </li>
+                                </ul>
+                            </li>
+
+
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="dropdown-item" href="pages/blog-category.html">Blog</a>
+
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
